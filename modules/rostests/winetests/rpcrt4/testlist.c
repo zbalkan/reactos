7452234a6f0bcf6925fde67248b1c1eba8998d77
Zafer Balkan <zafer@zaferbalkan.com>
2026-09-17 00:43:16 +0300

    [RPCRT4] Register NDR union conformance test

--- modules/rostests/winetests/rpcrt4/testlist.c
+++ modules/rostests/winetests/rpcrt4/testlist.c
@@ -6,6 +6,7 @@
 extern void func_cstub(void);
 extern void func_generated(void);
 extern void func_ndr_marshall(void);
+extern void func_ndr_union(void);
 extern void func_rpc(void);
 extern void func_rpc_async(void);
 extern void func_server(void);
@@ -15,6 +16,7 @@ const struct test winetest_testlist[] =
     { "cstub", func_cstub },
     { "generated", func_generated },
     { "ndr_marshall", func_ndr_marshall },
+    { "ndr_union", func_ndr_union },
     { "rpc", func_rpc },
     { "rpc_async", func_rpc_async },
     { "server", func_server },
